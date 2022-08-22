3d49f59986404c45e0fd64cb950417c89c06d03f
Karolin Rafalski <kr2174@caa.columbia.edu>
2022-08-22 09:38:28 -0400

    add css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ function App() {
     <div className="App">
       <header>
         <h1> Bark and Bowl Doggy Day Care</h1>
+        <h2>{new Date().toDateString()}</h2>
       </header>
+      <aside></aside>
+      <main></main>
     </div>
   );
 }
