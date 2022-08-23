535a0b82a52f608db473df2b2c8d7783985e56b2
Karolin Rafalski <kr2174@caa.columbia.edu>
2022-08-23 08:07:37 -0400

    draft complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
+import { dogsData } from "./data";
+import { useState } from "react";
+import DogDetails from "./DogDetails";
+
+// console.log(dogsData);
+
 function App() {
+  const [dogs, setDogs] = useState(dogsData);
+
+  function addDog() {
+    const rover = {
+      id: dogs.length + 50,
+      name: "Rover",
+      present: false,
+      grade: "100",
+      notes: "The goodest new dog",
+    };
+    setDogs([rover, ...dogs]);
+  }
+
+  function removeDog(dogID) {
+    const filteredDogArray = dogs.filter((dog) => dog.id !== dogID);
+    setDogs(filteredDogArray);
+  }
+
+  function updateDogAttendance(dogId) {
+    // Copy the dogs array so that the copy can be updated
+    const dogArray = [...dogs];
+    // Find the dog with the matching id number's array position
+    const index = dogArray.findIndex((dog) => dogId === dog.id);
+    // Access the dog's present property and update the value
+    // By using ! it will change a true value to false or a false value to true
+    dogArray[index].present = !dogArray[index].present;
+    // Put the updated array into setDogs to update the dogs array
+    setDogs(dogArray);
+  }
   return (
     <div className="App">
       <header>
         <h1> Bark and Bowl Doggy Day Care</h1>
-        <h2>{new Date().toDateString()}</h2>
       </header>
-      <aside></aside>
-      <main></main>
+      <main>
+        <button onClick={addDog}>Add a new dog</button>
+        <ul>
+          {dogs.map((dog) => {
+            return (
+              <li key={dog.id}>
+                <span
+                  onClick={() => updateDogAttendance(dog.id)}
+                  style={
+                    dog.present
+                      ? { textDecoration: "none" }
+                      : { textDecoration: "line-through" }
+                  }
+                >
+                  {dog.name}{" "}
+                </span>
+
+                <button onClick={() => removeDog(dog.id)}>remove</button>
+                <DogDetails dog={dog} />
+              </li>
+            );
+          })}
+        </ul>
+      </main>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export default [
+export const dogsData = [
   {
     id: 46,
     name: "Mitsy",
